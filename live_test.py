e526d0aa7421386ed29a183d41b534ae36eae2e2
Jack Fogerty <156630534+jackfogerty@users.noreply.github.com>
2025-04-10 22:34:18 -0400

    wrote code to display camera and have live emotion detection

--- live_test.py
+++ live_test.py
@@ -2,6 +2,7 @@ import cv2
 import cvlib as cv
 import joblib
 from PIL import Image
+import time
 
 #this function uses openCV to capture default camera, and then estimates emotion using the model
 def estimate_emotion(path):
@@ -9,27 +10,36 @@ def estimate_emotion(path):
 
     #selects defauly camera
     cap = cv2.VideoCapture(0)
-
+    emotion = None
+    last_prediction_time = 0
     while True:
         ret, frame = cap.read()
 
         faces, confidences = cv.detect_face(frame) #detecting faces in the frame
 
+        current_time = time.time()
+
         #crops each face in the frame, and predicts what emotion they are feeling, then prints that prediction
+        counter = 0
         for face, confidences in zip(faces, confidences):
             (start_x, start_y, end_x, end_y) = face
 
             cropped_face = frame[start_y:end_y, start_x:end_x]
 
-            face_resize = cv2.resize(cropped_face, (48, 48)).convert('L')
+            face_resize = cv2.resize(cropped_face, (48, 48))
 
-            flat_face = face_resize.flatten()
+            gray_face = cv2.cvtColor(face_resize, cv2.COLOR_BGR2GRAY)
 
-            emotion = loaded_model.predict([flat_face])[0]
+            flat_face = gray_face.flatten()
+
+            if current_time - last_prediction_time >= 3:
+                emotion = loaded_model.predict([flat_face])[0]
+                last_prediction_time = current_time
 
             label  = f'Emotion: {emotion}'
             cv2.rectangle(frame, (start_x, start_y), (end_x, end_y), (0, 255, 0), 2)
             cv2.putText(frame, label, (start_x, start_y - 10), cv2.FONT_HERSHEY_PLAIN, 0.8, (0, 255, 0), 2)
+            counter += 1
 
         cv2.imshow('Emotion Detection', frame)
 
@@ -41,4 +51,4 @@ def estimate_emotion(path):
     cv2.destroyAllWindows()
 
 if __name__ == "__main__":
-    estimate_emotion("Facial Recog Python/emotion_detector.pkl")
+    estimate_emotion("emotion_detector.pkl")
